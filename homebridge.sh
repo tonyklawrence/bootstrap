46de206732ba8e15d8c76b51e59f0f6ade3265fa
Tony Lawrence <code@tonylawrence.com>
2016-02-06 17:59:24 +0000

    suppress curl progress

--- homebridge.sh
+++ homebridge.sh
@@ -3,6 +3,6 @@
 # This is the homebridge bootstrap script. To install homebridge, you can
 # execute it in a terminal:
 #
-# sh -c "`curl https://raw.githubusercontent.com/tonyklawrence/bootstrap/master/homebridge.sh`"
+# sh -c "`curl -sS https://raw.githubusercontent.com/tonyklawrence/bootstrap/master/homebridge.sh`"
 
 sh -c "`curl -sS https://babushka.me/up`" </dev/null
