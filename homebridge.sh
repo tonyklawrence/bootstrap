7f293d436baf3c4f13fc0269df859980037786c7
Tony Lawrence <code@tonylawrence.com>
2016-02-06 17:46:48 +0000

    creating a bootstrap script for homebridge installations on a pi

---
+++ homebridge.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# This is the homebridge bootstrap script. To install homebridge, you can
+# execute it in a terminal:
+#
+# sh -c "`curl https://raw.githubusercontent.com/tonyklawrence/bootstrap/master/homebridge.sh`"
+
+echo "Hello"
